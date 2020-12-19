c4f4873545473c323c461ec73a43519a70048c27
eryawanir <mochyawn@gmail.com>
2020-12-19 20:17:34 +0700

    buat model dokter dan view pada master dokter

--- app/Controllers/Dokter.php
+++ app/Controllers/Dokter.php
@@ -2,14 +2,23 @@
 
 namespace App\Controllers;
 
+use App\Models\DokterModel;
+
 class Dokter extends BaseController
 {
     public function index()
 
     {
+        $dokterModel = new DokterModel();
+        $dokter = $dokterModel->findAll();
+
+
+
         $data = [
             'title' => ' List Dokter',
-            'page' => 'dokter'
+            'page' => 'dokter',
+            'dokter' => $dokter
+
         ];
 
         return view('pages/dokter', $data);
